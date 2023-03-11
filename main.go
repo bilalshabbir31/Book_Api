08a1d65197189fff7b87d79c6b0e47ded7550832
Bilal Shabbir <112929098+infinityloop031@users.noreply.github.com>
2023-03-11 19:22:28 +0500

    add GET route

--- main.go
+++ main.go
@@ -1,2 +1,33 @@
 package main
 
+import(
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"errors"
+)
+
+
+type book struct{
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Author string `json:"author"`
+	Quantity int `json:"quantity"`
+}
+
+
+var books=[]book{
+
+	{ID:"1" ,Title: "DLD",Author: "Rehan Saleem" ,Quantity: 50 },
+	{ID:"2" ,Title: "Programming Fundamentals" ,Author:"Sheraz" ,Quantity:  60},
+	{ID:"3" ,Title:"Machine Learning" ,Author:"Abdullah" ,Quantity: 100 },
+}
+
+func getBooks(c *gin.Context){
+	c.IndentedJSON(http.StatusOK,books)
+}
+
+func main(){
+	router :=gin.Default()
+	router.GET("/books",getBooks)
+	router.Run("localhost:8080")
+}
