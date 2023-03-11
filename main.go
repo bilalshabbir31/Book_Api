b5454f70f70f945b09d5da24db9cfcec296ffc6d
Bilal Shabbir <112929098+infinityloop031@users.noreply.github.com>
2023-03-11 19:56:38 +0500

    add more get routes

--- main.go
+++ main.go
@@ -26,8 +26,39 @@ func getBooks(c *gin.Context){
 	c.IndentedJSON(http.StatusOK,books)
 }
 
+func createBook(c *gin.Context){
+	var newbook book
+	if err:=c.BindJSON(&newbook); err!=nil{
+		return
+	}
+	books=append(books,newbook)
+	c.IndentedJSON(http.StatusCreated,newbook)
+}
+
+func bookbyid(c *gin.Context){
+	id :=c.Param("id")
+	book,err:=getBookById(id)
+
+	if err!=nil{
+		c.IndentedJSON(http.StatusNotFound,gin.H{"message":"book not found!"})
+		return
+	}
+	c.IndentedJSON(http.StatusOK,book)
+}
+
+func getBookById(id string)(*book,error ){
+	for i,b := range books{
+		if b.ID==id{
+			return &books[i], nil
+		}
+	}
+	return nil,errors.New("book not found")
+}
+
 func main(){
 	router :=gin.Default()
 	router.GET("/books",getBooks)
+	router.GET("/books/:id",bookbyid)
+	router.POST("/books",createBook)
 	router.Run("localhost:8080")
 }
